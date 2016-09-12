2fe019e9b5a3babd189228035c9fd2269d9fb3fa
thisray <gn01697933@gmail.com>
2016-09-12 11:17:54 +0800

    cht add

--- first_class/initLesson.R
+++ first_class/initLesson.R
@@ -2,3 +2,26 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
+
+library(magrittr)
+# assign("csvfile_path", 
+#        #.get_path("hcc_utf8.csv"),
+#        "/hcc_utf8.csv",
+#        envir = globalenv())
+
+
+
+# For compatibility with 2.2.21
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")},
+           warning = function(w) {file.path(find.package("swirl"),"Courses")},
+           silent=T
+  )
+}
+
+# Make path to csv available to user
+csvfile_path <- file.path(.get_course_path(),
+                      'swirl_cht_course',
+                      'first_class',
+                      'hcc_utf8.csv')
